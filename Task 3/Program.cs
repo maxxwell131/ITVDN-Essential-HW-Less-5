e6f19ca000f0abc43c17554f4ae9f3b129f07436
maxxwell131 <maxxwell131@yahoo.com>
2018-03-04 18:17:20 +0200

    Task 2 is finish

--- Task 3/Program.cs
+++ Task 3/Program.cs
@@ -17,7 +17,23 @@ namespace Task_3
         */
         static void Main(string[] args)
         {
+            MyMatrix m = new MyMatrix(4, 5);
+            m.Show();
 
+            Console.WriteLine(new string('-', 30));
+            Console.WriteLine("[1][2] = {0}", m[1, 2]);
+            Console.WriteLine("[6][2] = {0}", m[6, 2]);
+            m[0, 0] = 11;
+
+            Console.WriteLine(new string('-', 30));
+            m.ChengeSize(7, 6);
+            m.Show();
+
+            Console.WriteLine(new string('-', 30));
+            m.ShowPartly(1, 0, 4, 4);
+
+            //Delay
+            Console.ReadKey();
         }
     }
 }
